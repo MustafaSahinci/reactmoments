32f01de2441cb16e3a44247a72b77fb7e16a0bf7
Mustafa <mustafa_sahinci@hotmail.com>
2022-10-31 16:17:00 +0000

    add files

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from '../styles/Avatar.module.css'
 
-const Avatar = (props) => {
-  const {src, height=45, text} = props;
-
+const Avatar = ({src, height=45, text}) => {
   return (
-    <div></div>
+    <span>
+      <img className={styles.Avatar} src={src} height={height} width={height} alt="avatar" />
+      {text}
+    </span>
   )
 }
 
