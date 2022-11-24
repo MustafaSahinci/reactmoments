bb8f20e960489f343337e31e8a8e43a1115143ba
Mustafa <mustafa_sahinci@hotmail.com>
2022-11-24 22:03:02 +0000

    cleaning up

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,3 +1,20 @@
+import { rest } from "msw"
+
 const baseURL = "https://drf-api-react.herokuapp.com/"
 
-export const handlers = []
+export const handlers = [
+    rest.get(`$(baseURL)dj-rest-auth/user/`, (req, res, ctx) => {
+        return res(ctx.json({
+            pk: 1,
+            username: "hassan",
+            email: "",
+            first_name: "",
+            last_name: "",
+            profile_id: 1,
+            profile_image: "https://res.cloudinary.com/dxq7tud8l/image/upload/v1/media/../default_profile_e296yh"
+            }));
+    }),
+    rest.post(`$(baseURL)dj-rest-auth/logout/`, (req, res, ctx) => {
+        return res(ctx.status(200))
+    }),
+];
